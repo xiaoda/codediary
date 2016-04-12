752be6c44183a7795543157058840c0d7477be85
xiaoda <lqdai.xiaoda@gmail.com>
2016-04-12 23:52:42 +0800

    common ports search hit color & add some items

--- page/common_ports/res/index.js
+++ page/common_ports/res/index.js
@@ -5,6 +5,8 @@ var PORTS_DATA = [
     {name: 'Django', port: 8000},
     {name: 'Expressjs', port: 3000},
     {name: 'FastCGI', port: 9000},
+    {name: 'FTP', port: 20},
+    {name: 'FTP', port: 21},
     {name: 'Ghostjs', port: 2368},
     {name: 'Jekyll', port: 4000},
     {name: 'Koajs', port: 3000},
@@ -14,8 +16,10 @@ var PORTS_DATA = [
     {name: 'MySQL', port: 3306},
     {name: 'Nginx', port: 80},
     {name: 'Oracle', port: 1521},
+    {name: 'PPTP', port: 1723},
     {name: 'Redis', port: 6379},
     {name: 'Ruby on Rails', port: 3000},
+    {name: 'Socks', port: 1080},
     {name: 'SQL Server', port: 1433},
     {name: 'SSDB', port: 8888},
 ];
@@ -24,7 +28,7 @@ var RESULT = {
 }
 
 /* 根据数据生成表格 */
-function generateTable(id, data){
+function generateTable(id, data, regExp){
     var $table = $('table#' + id);
     if(!$table.length) return false;
     var template = '<tr><td>{{port}}</td><td>{{name}}</td></tr>';
@@ -32,10 +36,18 @@ function generateTable(id, data){
     var tableData = _.groupBy(data, 'port');
     for(var i in tableData){
         var tmpNames = [];
-        for(var j in tableData[i]){
-            tmpNames.push(tableData[i][j]['name']);
+        var tmpPort = i;
+        if(regExp) {
+            tmpPort = tmpPort.replace(regExp, '<span class="hit">$&</span>');
         }
-        html += template.replace('{{port}}', i).replace('{{name}}', tmpNames.join(' , '));
+        for(var j in tableData[i]){
+            var tmpName = tableData[i][j]['name'];
+            if(regExp) {
+                tmpName = tmpName.replace(regExp, '<span class="hit">$&</span>');
+            }
+            tmpNames.push(tmpName);
+        }
+        html += template.replace('{{port}}', tmpPort).replace('{{name}}', tmpNames.join(' , '));
     }
     $table.find('tbody').empty().html(html);
 
@@ -47,7 +59,7 @@ $('body').on('input', '#inputSearch', function(){
     var search = $(this).val();
     if(search){
         var data = [];
-        var tmpRegExp = new RegExp(search.toLowerCase(), 'i');
+        var tmpRegExp = new RegExp(search.toLowerCase(), 'ig');
         _.each(PORTS_DATA, function(value, key, list){
             _.each(value, function(v, k, l){
                 if(tmpRegExp.test(String(v).toLowerCase())){
@@ -55,7 +67,7 @@ $('body').on('input', '#inputSearch', function(){
                 }
             });
         });
-        RESULT.table = generateTable('portsTable', data);
+        RESULT.table = generateTable('portsTable', data, tmpRegExp);
     }else{
         RESULT.table = generateTable('portsTable', PORTS_DATA);
     }
